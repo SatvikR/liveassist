360dd540b402e8f22e16ace3f38314fe510bc7e8
Satvik Reddy <reddy.satvik@gmail.com>
2021-07-02 20:52:27 -0700

    ostium: remove redundant console.log

--- ostium/src/lib/api.ts
+++ ostium/src/lib/api.ts
@@ -1,6 +1,5 @@
 import { HTTPClient } from "@liveassist/liber";
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-console.log(apiUrl);
 
 export const api = !!apiUrl ? new HTTPClient(apiUrl) : new HTTPClient();
